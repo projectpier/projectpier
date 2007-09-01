8ac234722eeadf0e03b63262342b2691762f7079
projectpier <projectpier@5e44df70-d631-0410-a02e-f1bdc1e0250e>
2007-09-01 04:00:04 +0000

    Update MailProxy.php again, last entry had a bug.
    git-svn-id: https://projectpier.svn.sourceforge.net/svnroot/projectpier/ProjectPier/Trunk@62 5e44df70-d631-0410-a02e-f1bdc1e0250e

--- library/swift/lib/Swift/Stream/MailProxy.php
+++ library/swift/lib/Swift/Stream/MailProxy.php
@@ -367,7 +367,8 @@ class Swift_Stream_MailProxy implements Swift_IStream
 		@ini_set('sendmail_from', $this->from);
 		$extra = "-oi -f ".$this->from;
 		$sent = @mail(
-		  str_replace("\n", ", implode(', ', $this->getTo($string))),
+		  //implode(', ', $this->getTo($string)),
+	          str_replace("\n", '', implode(', ', $this->getTo($string))),
 		  $this->getSubject($string),
 		  $this->getBody($string),
 		  $this->getHeaders($string),
